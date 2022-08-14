59a1d45cd876ac3018b88ee56238fbbd0b92da4a
Gokhan Yucegok <gyucegok@gmail.com>
2022-08-14 21:54:21 +0300

    v0.14

--- src/main.go
+++ src/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
-		rw.Write([]byte(fmt.Sprintf("Hello World!\n")))
+		rw.Write([]byte(fmt.Sprintf("Hello World!!!\n")))
 	})
 	http.ListenAndServe(":80", nil)
 }
