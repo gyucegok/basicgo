763ef80a04cc005dbacefd80263311bf37b27c15
Gokhan Yucegok <gyucegok@gmail.com>
2023-04-19 14:38:54 +0000

    v0.31

--- src/main.go
+++ src/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
-		rw.Write([]byte(fmt.Sprintf("Hello Gokhan!\n")))
+		rw.Write([]byte(fmt.Sprintf("Hello TIME!\n")))
 	})
 	http.ListenAndServe(":80", nil)
 }
